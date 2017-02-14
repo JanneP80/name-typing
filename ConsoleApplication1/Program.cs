8e465a63b0759f1beb6889281f70a94ef39fc4f5
JanneP80 <janne.puolitaival@student.saimia.fi>
2017-02-14 15:03:17 +0200

    Adding two new projects.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type number: ");
+            int iNro = int.Parse(Console.ReadLine()); // numbers are also characters, change to interger
+
+            if (iNro < 0) // { } Lohkomerkintä on hyvää ohjelmointityyliä
+            {
+                Console.WriteLine("Number {0} is negative.", iNro);
+            }
+            else
+            if (iNro > 0) // larger than zero
+            {
+                Console.WriteLine("Number {0} is positive.", iNro);
+            }
+            else
+            if (iNro == 0) // is zero
+            {
+                Console.WriteLine("Number {0} is zero.", iNro);
+            }
+            else return;
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ name-typing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace name_typing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("type your firstname: ");
+            string strFirstName = Console.ReadLine();
+
+            Console.WriteLine("type your lastname: ");
+            string strLastName = Console.ReadLine();
+
+            //Console.WriteLine("Your name is " + strName);
+            Console.WriteLine("Your name is {0} and lastname {1}", strFirstName, strLastName);
+            Console.ReadKey();
+        }
+    }
+}
